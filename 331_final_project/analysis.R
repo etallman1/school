edf7dd86dea4ada9831a785ee1b30e44ad78032a
Em T <etallman@uw.edu>
2023-05-16 20:20:00 -0700

    saved plots

--- 331_final_project/analysis.R
+++ 331_final_project/analysis.R
@@ -47,7 +47,9 @@ dist_plot <- ggplot(data, aes(x = category, y = score, col = category, fill = ca
   labs(x = "Word Category", y = "Total Recall", col = "Word Categories", title = "Total Recall for Each Word Category") +
   theme(axis.title.x = element_blank(),) +
   guides(fill = "none") 
-dist_plot
+
+ggsave("dist_plot.png", dist_plot)
+
 
 data_means <- data %>% group_by(category) %>%
   summarize(mean_score = mean(score), category = unique(category), se = sd(score)/sqrt(nrow(.))) 
@@ -59,7 +61,9 @@ avg_bar_plot <- ggplot(data_means, aes(x = category, y = mean_score, fill = cate
   labs(x = "Word Category", y = "Mean Recall", fill = "Word Categories", title = "Mean Recall for Each Word Category") +
   theme_hc() +
   theme(axis.title.x = element_blank())
-avg_bar_plot
+
+
+ggsave("avg_barplot.png", avg_bar_plot)
 
 data_sep <- read.csv("final_data.csv") %>%
   rename("score" = "ï..score") %>%
@@ -73,7 +77,8 @@ avg_line_plot <- ggplot(data_sep, aes(x = concreteness, y = mean_score, color = 
   labs(x = "Concretness", y = "Mean Recall", color = "Valence", title = "Mean Recall for Each Word Category") +
   theme_hc() +
   ylim(8, 9)
-avg_line_plot
+
+ggsave("avg_line_plot.png", avg_line_plot)
 
 # ggplot(cleandata_avg, aes(y = MeanAlpha, x = language, col = language, fill = language)) +
 #   geom_boxplot(alpha = 0.25, position = position_dodge(width = 0.75)) +
